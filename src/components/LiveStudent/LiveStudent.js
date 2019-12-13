a60c8391e234df5c8c6ee1c7c40de2dbd0c6643e
Emilee Mitchell <emcakes@gmail.com>
2019-12-13 15:34:19 -0600

    added prop-types

---
+++ src/components/LiveStudent/LiveStudent.js
@@ -0,0 +1,1 @@
+// a card of your choosing that gets called from the SharkTank component inside a map.
---
+++ src/helpers/data/studentData.js
@@ -0,0 +1,149 @@
+const students = [
+  {
+    id: 'student1',
+    firstName: 'Emilee',
+    lastName: 'Mitchell',
+    isDead: false,
+  },
+  {
+    id: 'student2',
+    firstName: 'Alesha',
+    lastName: 'Reed',
+    isDead: false,
+  },
+  {
+    id: 'student3',
+    firstName: 'Ashley',
+    lastName: 'Claiborne',
+    isDead: false,
+  },
+  {
+    id: 'student4',
+    firstName: 'Charity',
+    lastName: 'Bunyon',
+    isDead: false,
+  },
+  {
+    id: 'student5',
+    firstName: 'Connor',
+    lastName: 'Sullivan',
+    isDead: false,
+  },
+  {
+    id: 'student6',
+    firstName: 'Crystal',
+    lastName: 'Broach',
+    isDead: false,
+  },
+  {
+    id: 'student7',
+    firstName: 'Denise',
+    lastName: 'Baker',
+    isDead: false,
+  },
+  {
+    id: 'student8',
+    firstName: 'Evan ',
+    lastName: 'Grabenstein',
+    isDead: false,
+  },
+  {
+    id: 'student9',
+    firstName: 'Gabriel ',
+    lastName: 'Seals',
+    isDead: false,
+  },
+  {
+    id: 'student10',
+    firstName: 'Ivan ',
+    lastName: 'Phelps',
+    isDead: false,
+  },
+  {
+    id: 'student11',
+    firstName: 'Jacob',
+    lastName: 'Best-Wittenberg',
+    isDead: false,
+  },
+  {
+    id: 'student12',
+    firstName: 'Jamie ',
+    lastName: 'Phillips',
+    isDead: false,
+  },
+  {
+    id: 'student13',
+    firstName: 'Jasmine ',
+    lastName: 'Williams',
+    isDead: false,
+  },
+  {
+    id: 'student14',
+    firstName: 'John ',
+    lastName: 'Key',
+    isDead: false,
+  },
+  {
+    id: 'student15',
+    firstName: 'John ',
+    lastName: 'Thielman',
+    isDead: false,
+  },
+  {
+    id: 'student16',
+    firstName: 'John',
+    lastName: 'Johnson',
+    isDead: false,
+  },
+  {
+    id: 'student17',
+    firstName: 'Kelsey',
+    lastName: 'Creel',
+    isDead: false,
+  },
+  {
+    id: 'student18',
+    firstName: 'Laura',
+    lastName: 'Collins',
+    isDead: false,
+  },
+  {
+    id: 'student19',
+    firstName: 'Maggie',
+    lastName: 'Greene',
+    isDead: false,
+  },
+  {
+    id: 'student20',
+    firstName: 'Maria',
+    lastName: 'Brock',
+    isDead: false,
+  },
+  {
+    id: 'student21',
+    firstName: 'Monica',
+    lastName: 'Djunaidi',
+    isDead: false,
+  },
+  {
+    id: 'student22',
+    firstName: 'Randy',
+    lastName: 'Tate',
+    isDead: false,
+  },
+  {
+    id: 'student23',
+    firstName: 'Raymond',
+    lastName: 'Arceneaux',
+    isDead: false,
+  },
+];
+
+
+// This file should have an array at the top called students
+// The array should have one student object for every student in the class (if you don't remember the other students in your class thats SUPER awkward but everyone is listed in classtracker so go there and figure it out)
+// Each student object should have the following keys: id, firstName, lastName, isDead
+// The isDead key should be set to false for everyone on load
+// This file should export a function called livingStudents that returns an array of the students who have isDead === false
+// This file should export a function called dearlyBeloved that returns an array of the students who have isDead === true
+// This file should export a function called followTheLight that takes in an id, finds that student, and changes isDead === true.
