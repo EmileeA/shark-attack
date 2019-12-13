1062b5bbf3ff16c360d1582f0459d4592b77a25f
Emilee Mitchell <emcakes@gmail.com>
2019-12-13 15:54:24 -0600

    WIP

---
+++ src/components/SharkTank/SharkTank.js
@@ -0,0 +1,15 @@
+// the pool that all living students are swimming in
+import React from 'react';
+import './SharkTank.scss';
+
+class SharkTank extends React.Component {
+  render() {
+    return (
+      <div className="sharkTank row d-flex justify-content-center">
+        <h4 id="tankLabel">Shark Tank</h4>
+      </div>
+    );
+  }
+}
+
+export default SharkTank;
