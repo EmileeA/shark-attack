755a17663d75fb25007d87e9e27c575bebdb8a14
Emilee Mitchell <emcakes@gmail.com>
2019-12-17 17:28:26 -0600

    WIP

--- src/components/SharkTank/SharkTank.js
+++ src/components/SharkTank/SharkTank.js
@@ -1,11 +1,25 @@
 // the pool that all living students are swimming in
 import React from 'react';
+import PropTypes from 'prop-types';
+import LiveStudent from '../LiveStudent/LiveStudent';
 import './SharkTank.scss';
+import studentShape from '../../helpers/propz/studentShape';
 
 class SharkTank extends React.Component {
+  static propTypes = {
+    students: PropTypes.arrayOf(studentShape.studentShape),
+  }
+
   render() {
+    const { students } = this.props;
+
+    const studentCards = students.map((student) => <LiveStudent key={student.id} student={student} />);
+
     return (
       <div className="sharkTank row col-3">
+        <div className="studentContainer row">
+        {studentCards}
+        </div>
         <h3 id="tankId">Shark Tank</h3>
       </div>
     );
